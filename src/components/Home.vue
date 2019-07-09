3a940ac15b653caa467f1b6d15a7ed35fd5c4573
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-07-09 13:31:41 -1000

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Trade of View your Portfolio</h1>
+        <h6>You may Save & Load your Data</h6>
+        <h6>Click on 'End Day' to being a New Day!</h6>
+        <hr>
+        <p>Your Funds: {{ funds }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        funds(){
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
